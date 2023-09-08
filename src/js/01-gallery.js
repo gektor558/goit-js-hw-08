c88de643e9bbd383492fe909697427fb9c221432
gektor558 <danil.trigub@gmail.com>
2023-09-08 20:04:58 +0200

    hw-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    galleryList: document.querySelector('.gallery'),
+};
+
+
+
+function galleryMarkup(array) {
+  const markup = array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    })
+    .join('');
+
+  return markup;
+}
+
+function renderGalleryList(galleryArr) {
+  const markup = galleryMarkup(galleryArr);
+    refs.galleryList.insertAdjacentHTML('beforeend', markup);
+}
+renderGalleryList(galleryItems);
+
+
+new simpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250, });
