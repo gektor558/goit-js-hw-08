d6275fd9b8aa18f2f5e36c25cefdcc91355dddcf
gektor558 <danil.trigub@gmail.com>
2023-09-26 18:27:27 +0200

    hw-03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 import simpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,65 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    formUserEl: document.querySelector('.feedback-form'),
+    localStorageKey: "feedback-form-state",
+}
+
+refs.saveUserEmailEl = refs.formUserEl.children[0].firstElementChild;
+refs.saveUserMessageEl = refs.formUserEl.children[1].firstElementChild;
+
+const inputData = throttle(saveToLocalStorage, 500);
+
+function saveToLocalStorage () {
+    try {
+        const key = refs.localStorageKey;
+        const userData = {
+            email: refs.saveUserEmailEl.value,
+            message: refs.saveUserMessageEl.value,
+        };
+        const serializedState = JSON.stringify(userData);
+        localStorage.setItem(key, serializedState);
+    } catch (error) {
+        console.error("Set state error: ", error.message);
+    };
+};
+
+refs.saveUserEmailEl.addEventListener('input', inputData);
+refs.saveUserMessageEl.addEventListener('input', inputData);
+
+
+const savedUserData = getFromLocalStorage();
+refs.saveUserEmailEl.value = savedUserData.email;
+refs.saveUserMessageEl.value = savedUserData.message;
+
+function getEmptyUserData() {
+    return {
+        email: '',
+        message: '',
+    };
+};
+function getFromLocalStorage() {
+    try {
+        const serializedState = localStorage.getItem(refs.localStorageKey);
+        return serializedState === null ? getEmptyUserData() : JSON.parse(serializedState);
+    } catch (error) {
+       console.error("Get state error: ", error.message);
+       return getEmptyUserData();
+    };  
+};
+
+//3 step
+refs.formUserEl.addEventListener("submit", onDataSubmit);
+
+function onDataSubmit (event) {
+    event.preventDefault();
+    
+    const userData = {
+        email: refs.saveUserEmailEl.value,
+        message: refs.saveUserMessageEl.value,
+    };
+
+    refs.formUserEl.reset();
+    localStorage.removeItem(refs.localStorageKey);
+    console.log(userData);
+};
