13f391aa13be59fd96105489d31986cce90dbc7e
gektor558 <danil.trigub@gmail.com>
2023-09-27 19:06:01 +0200

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,22 +1,60 @@
-import { galleryItems } from './gallery-items.js';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import SimpleLightbox from 'simplelightbox'; // Зверніть увагу, що імпортується SimpleLightbox
+// import { galleryItems } from './gallery-items.js';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// import SimpleLightbox from 'simplelightbox'; // Зверніть увагу, що імпортується SimpleLightbox
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-const galleryList = document.querySelector('.gallery');
+// const galleryList = document.querySelector('.gallery');
 
-const markup = galleryItems.map(el => {
-  return `<li class="gallery__item">
-  <a class="gallery__link" href="${el.original}">
-     <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
-  </a>
+// const markup = galleryItems.map(el => {
+//   return `<li class="gallery__item">
+//   <a class="gallery__link" href="${el.original}">
+//      <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+//   </a>
+// </li>`;
+// }).join('\n');
+
+// galleryList.innerHTML = markup;
+
+// let lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
+
+
+// Add imports above this line
+import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+
+
+const refs = {
+    galleryList: document.querySelector('.gallery'),
+};
+
+
+
+function galleryMarkup(array) {
+  const markup = array
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
 </li>`;
-}).join('\n');
+    })
+    .join('');
 
-galleryList.innerHTML = markup;
+  return markup;
+}
 
-let lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
+function renderGalleryList(galleryArr) {
+  const markup = galleryMarkup(galleryArr);
+    refs.galleryList.insertAdjacentHTML('beforeend', markup);
+}
+renderGalleryList(galleryItems);
+
+
+new simpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250, });
