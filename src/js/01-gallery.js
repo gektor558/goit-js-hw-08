5e3ce14962cb5c0328d2197658dec6656d8a1deb
gektor558 <danil.trigub@gmail.com>
2023-09-27 19:09:38 +0200

    fixxxxx

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,31 +1,7 @@
-// import { galleryItems } from './gallery-items.js';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import SimpleLightbox from 'simplelightbox'; // Зверніть увагу, що імпортується SimpleLightbox
-
-// console.log(galleryItems);
-
-// const galleryList = document.querySelector('.gallery');
-
-// const markup = galleryItems.map(el => {
-//   return `<li class="gallery__item">
-//   <a class="gallery__link" href="${el.original}">
-//      <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
-//   </a>
-// </li>`;
-// }).join('\n');
-
-// galleryList.innerHTML = markup;
-
-// let lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
-
-
 // Add imports above this line
 import simpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items';
+import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
 
@@ -57,4 +33,4 @@ function renderGalleryList(galleryArr) {
 renderGalleryList(galleryItems);
 
 
-new simpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250, });
+let simpleLightBox = new simpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250, });
